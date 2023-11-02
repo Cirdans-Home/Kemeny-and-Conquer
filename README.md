65d8277db3021b91d815b6d10b96b7b623e69b2d
Cirdans-Home <f.durastante@na.iac.cnr.it>
2023-11-02 22:04:26 +0100

    HODLR implementation of the algorithm

--- README.md
+++ README.md
@@ -21,3 +21,28 @@ Some of the tests make use of the stochastic trace estimator called Hutch++. The
 }
 ```
 
+## hm-toolbox
+
+One of the implemented version of the recursive algorithm for the approximation
+of the Kemeny constant uses the hm-toolbox by Massei, Robol and Kressner. If you
+use that version of the algorithm for estmating the Kemeny constant, you should
+also cite the original article:
+
+```{bibtex}
+@article {MR4082285,
+    AUTHOR = {Massei, Stefano and Robol, Leonardo and Kressner, Daniel},
+     TITLE = {hm-toolbox: {MATLAB} software for {HODLR} and {HSS} matrices},
+   JOURNAL = {SIAM J. Sci. Comput.},
+  FJOURNAL = {SIAM Journal on Scientific Computing},
+    VOLUME = {42},
+      YEAR = {2020},
+    NUMBER = {2},
+     PAGES = {C43--C68},
+      ISSN = {1064-8275,1095-7197},
+   MRCLASS = {65F99 (15A03 65F05 65F08 65F10 65F50 65Y15)},
+  MRNUMBER = {4082285},
+MRREVIEWER = {Bruno\ Carpentieri},
+       DOI = {10.1137/19M1288048},
+       URL = {https://doi.org/10.1137/19M1288048},
+}
+```
---
+++ src/recursivekemenyhodlr.m
@@ -0,0 +1,42 @@
+function k = recursivekemenyhodlr(P,pi)
+%KEMENYRANKSTRUCTURE uses rank-structured matrices to perform the divide
+%and conquer strategy.
+%   INPUT:  P stochastic matrix, if the matrix is not in hodlr format it
+%           get converted on input
+%           pi stationary vector of the matrix P
+%   OUTPUT: k Kemeny constant (approximation within hodlr format accuracy)
+
+if ~isa(P,"hodlr")
+    P = hodlr(P);
+end
+
+m = size(P.A11,1);
+n = size(P,1);
+
+if n <= 500
+    k = kemenydirect(P);
+    return
+else
+    pi1 = pi(1:m)/sum(pi(1:m));
+    pi2 = pi(m+1:n)/sum(pi(m+1:n));
+    I2 = hodlr(speye(m,m));
+    I1 = hodlr(speye(n-m,n-m));
+    % Build censored chains
+    [L,U] = lu(I1-P.A22);
+    P1 = P.A11 + hodlr('low-rank',P.U12,(P.V12.'*hodlr('low-rank',U\(L\P.U21),P.V21)).');
+    P2 = P.A22 + hodlr('low-rank',P.U21,(P.V21.'*hodlr('low-rank',(I2-P.A11)\P.U12,P.V12)).');
+    % Compute theta
+    x = U\(L\ones(n-m,1));
+    y = (I2 - P1 - hodlr('low-rank',ones(size(pi1)),pi1))\(P.U12*(P.V12'*x));
+    y = x+U\(L\((P.U21*(P.V21'*y))));
+    theta = pi2'*(x+y);
+    gamma = sum(pi(1:m))*theta-sum(pi(m+1:n));
+    k1 = kemenyrankstructure(P1,pi1);
+    k2 = kemenyrankstructure(P2,pi2);
+    % Kemeny constant by update
+    k = k1+k2+gamma;
+end
+
+
+
+end
